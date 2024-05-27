e0ec51b886270200d3f4eec0a477ee0c74c05449
mrskunky69 <40913528+mrskunky69@users.noreply.github.com>
2024-05-27 10:11:33 +0100

    Delete rsg-notes directory

--- rsg-notes/config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.Debug = false
-Config.Prop = `p_paper02x`
-
--- rsg-notes/installation/shared_items.lua
+++
@@ -1,2 +0,0 @@
-    -- Items
-    ['notepad']      = {['name'] = 'notepad',      ['label'] = 'Notepad',       ['weight'] = 50,  ['type'] = 'item', ['image'] = 'notepad.png',      ['unique'] = true, ['useable'] = true,   ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = ''},
